91ca1c1d15c848df4a60aa8586de7c2aab11c0aa
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-07-11 13:24:54 +0200

    refactor: reduce dependencies

--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
 )
+
+type Config struct {
+	Hi []string `yaml:"hi"`
+}
 
 func main() {
 	log.SetFlags(0)
 	var configPath string
-	pflag.StringVar(&configPath, "config", "config.yaml", "config file")
-	pflag.Parse()
-	viper.SetConfigFile(configPath)
-	if err := viper.ReadInConfig(); err != nil {
+	flag.StringVar(&configPath, "config", "config.yaml", "config file")
+	flag.Parse()
+	log.Println(readConfig(configPath))
+}
+
+func readConfig(path string) (*Config, error) {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(b, &config); err != nil {
 		log.Fatalln(err)
 	}
-	var config any
-	if err := viper.Unmarshal(&config, nil); err != nil {
-		log.Fatalln(err)
-	}
-	log.Println(config)
+
+	return &config, nil
 }
