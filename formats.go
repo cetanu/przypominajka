8c436e49c5b25fb50b9fd7bbcb190d96f3370a90
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-07-12 12:18:18 +0200

    refactor: make formats nicer

--- formats.go
+++ formats.go
@@ -9,13 +9,13 @@ const (
 
 const (
 	// formatSingular is populated with: name and event type format.
-	formatSingular = "%s ma dziś %s!"
+	formatSingular = "%s obchodzi dziś %s!"
 	// formatSingular is populated with: name, surname, and event type format.
-	formatSingularSurname = "%s %s ma dziś %s!"
+	formatSingularSurname = "%s %s obchodzi dziś %s!"
 	// formatMessagePlural is populated with: names[0], names[1], and event type format.
-	formatMessagePlural = "%s i %s mają dziś %s!"
+	formatMessagePlural = "%s i %s obchodzą dziś %s!"
 	// formatMessagePluralSurname is populated with: names[0], names[1], surname, and event type format.
-	formatMessagePluralSurname = "%s i %s %s mają dziś %s!"
+	formatMessagePluralSurname = "%s i %s %s obchodzą dziś %s!"
 	// formatDone is populated with: caller's username and edited message text.
 	formatDone = "_✅ %s złożył(a) życzenia_\n\n%s"
 	// formatListLine is populated with: day, month, and event.
