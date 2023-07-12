4e4771911cbd148d5eba9161ecefd27b10f1a02b
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-07-12 22:25:14 +0200

    refactor: prepare for command handling

--- bot.go
+++ bot.go
@@ -35,26 +35,36 @@ func (b *bot) send(e event) error {
 }
 
 func (b *bot) listen() {
-	var err error
 	u := tg.NewUpdate(0)
 	u.Timeout = 60
 	for update := range b.api.GetUpdatesChan(u) {
-		if update.CallbackQuery != nil {
-			if _, err := b.api.Request(tg.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)); err != nil {
-				log.Println("ERROR", "failed to receive callback:", err)
-				continue
-			}
-
-			switch update.CallbackQuery.Data {
-			case dataDone:
-				err = b.callbackDone(update.CallbackQuery)
-			}
-			if err != nil {
+		if update.FromChat().ID != b.chatID {
+			continue
+		}
+		switch {
+		case update.CallbackQuery != nil:
+			if err := b.handleCallback(update); err != nil {
 				log.Println("ERROR", err)
 				continue
 			}
+		case update.Message.IsCommand():
+			switch update.Message.Command() {
+			case "next":
+				// TODO:
+			}
 		}
 	}
+}
+
+func (b *bot) handleCallback(update tg.Update) error {
+	if _, err := b.api.Request(tg.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)); err != nil {
+		return fmt.Errorf("failed to receive callback: %w", err)
+	}
+	switch update.CallbackData() {
+	case dataDone:
+		return b.callbackDone(update.CallbackQuery)
+	}
+	return nil
 }
 
 func (b *bot) callbackDone(cq *tg.CallbackQuery) error {
