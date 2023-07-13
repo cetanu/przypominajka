4ad433bc3e809d318fc13500e21b3b88ff655730
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-07-13 21:28:38 +0200

    refactor: store YAML path

--- storage/yaml.go
+++ storage/yaml.go
@@ -9,24 +9,27 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type YAML map[time.Month]map[int]models.Events
+type YAML struct {
+	path string
+	data map[time.Month]map[int]models.Events
+}
 
-var _ Interface = YAML{}
+var _ Interface = (*YAML)(nil)
 
-func NewYAML(path string) (YAML, error) {
+func NewYAML(path string) (*YAML, error) {
 	b, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
-	var y YAML
-	if err := yaml.Unmarshal(b, &y); err != nil {
+	y := &YAML{path: path}
+	if err := yaml.Unmarshal(b, &y.data); err != nil {
 		return nil, err
 	}
-	for m, month := range y {
+	for m, month := range y.data {
 		for d, day := range month {
 			for i, e := range day {
-				y[m][d][i].Month = m
-				y[m][d][i].Day = d
+				y.data[m][d][i].Month = m
+				y.data[m][d][i].Day = d
 				if err := e.Validate(); err != nil {
 					return nil, err
 				}
@@ -36,11 +39,11 @@ func NewYAML(path string) (YAML, error) {
 	return y, nil
 }
 
-func (y YAML) String() string {
+func (y *YAML) String() string {
 	lines := []string{}
 	for m := time.January; m <= time.December; m++ {
 		for d := 1; d <= 31; d++ {
-			if events, ok := y[m][d]; ok {
+			if events, ok := y.data[m][d]; ok {
 				lines = append(lines, events.String())
 			}
 		}
@@ -48,6 +51,6 @@ func (y YAML) String() string {
 	return strings.Join(lines, "\n")
 }
 
-func (y YAML) At(t time.Time) (models.Events, error) {
-	return y[t.Month()][t.Day()], nil
+func (y *YAML) At(t time.Time) (models.Events, error) {
+	return y.data[t.Month()][t.Day()], nil
 }
