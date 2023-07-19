e8a8611e290d233829a769c268a87d81129adcb7
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-07-19 20:21:18 +0200

    feat: add wizard.Delete boilerplate

---
+++ bot/wizard/delete.go
@@ -0,0 +1,42 @@
+package wizard
+
+import (
+	"context"
+
+	"git.sr.ht/~tymek/przypominajka/storage"
+	tg "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type Delete struct {
+	id   string
+	done context.CancelFunc
+}
+
+var _ Interface = (*Delete)(nil)
+
+func (d *Delete) ID() string {
+	return d.id
+}
+
+func (d *Delete) Name() string {
+	return "delete"
+}
+
+func (d *Delete) Active() bool {
+	return false
+}
+
+func (d *Delete) start(id string, done context.CancelFunc, update tg.Update) tg.Chattable {
+	d.id = id
+	d.done = done
+	return nil
+}
+
+func (d *Delete) Next(s storage.Interface, update tg.Update) (tg.Chattable, Consume, error) {
+	return nil, nil, nil
+}
+
+func (d *Delete) Reset() {
+	d.id = ""
+	d.done = nil
+}
